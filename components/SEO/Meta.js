7df46455a06355b04f8437202eef535b12738572
Nikola Dimitrov <104311364+NikolaDimitro@users.noreply.github.com>
2026-06-22 14:03:29 +0300

    Fix: Correct JSX comments syntax in Meta component and update content to Nikola Dimitro

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -4,44 +4,44 @@ import Head from 'next/head';
 export default function Meta() {
     return (
         <Head>
-           /* Primary Meta Tags */
-            <title>Vivek Patel Portfolio - Computer Engineering Student</title>
+            {/* Primary Meta Tags */}
+            <title>Nikola Dimitro Portfolio - Computer Engineering Student</title>
             <meta charSet="utf-8" />
-            <meta name="title" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            <meta name="title" content="Nikola Dimitro Portfolio - Computer Engineering Student" />
             <meta name="description"
-                content="Vivek Patel's (vivek9patel) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
-            <meta name="author" content="Vivek Patel (vivek9patel)" />
+                content="Nikola Dimitro's Personal Portfolio Website. Made with Ubuntu 20.04 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Nikola Dimitro" />
             <meta name="keywords"
-                content="vivek9patel, vivek9patel's portfolio, vivek9patel linux, ubuntu portfolio, vivek patel protfolio,vivek patel computer, vivek patel, vivek ubuntu, vivek patel ubuntu portfolio" />
+                content="nikola dimitro, nikola dimitro portfolio, linux portfolio, ubuntu portfolio, nikola dimitro computer, nikola dimitro ubuntu portfolio" />
             <meta name="robots" content="index, follow" />
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
             <meta name="language" content="English" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="theme-color" content="#E95420" />
 
-            /* Search Engine */
+            {/* Search Engine */}
             <meta name="image" content="images/logos/fevicon.png" />
-            /* Schema.org for Google */
-            <meta itemProp="name" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            {/* Schema.org for Google */}
+            <meta itemProp="name" content="Nikola Dimitro Portfolio - Computer Engineering Student" />
             <meta itemProp="description"
-                content="Vivek Patel's (vivek9patel) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+                content="Nikola Dimitro's Personal Portfolio Website. Made with Ubuntu 20.04 (Linux) theme by Next.js and Tailwind CSS." />
             <meta itemProp="image" content="images/logos/fevicon.png" />
-            /* Twitter */
+            {/* Twitter */}
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:title" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            <meta name="twitter:title" content="Nikola Dimitro Portfolio - Computer Engineering Student" />
             <meta name="twitter:description"
-                content="Vivek Patel's (vivek9patel) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
-            <meta name="twitter:site" content="vivek9patel" />
-            <meta name="twitter:creator" content="vivek9patel" />
+                content="Nikola Dimitro's Personal Portfolio Website. Made with Ubuntu 20.04 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="NikolaDimitro" />
+            <meta name="twitter:creator" content="NikolaDimitro" />
             <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
-            /* Open Graph general (Facebook, Pinterest & Google+) */
-            <meta name="og:title" content="Vivek Patel Portfolio - Computer Engineering Student" />
+            {/* Open Graph general (Facebook, Pinterest & Google+) */}
+            <meta name="og:title" content="Nikola Dimitro Portfolio - Computer Engineering Student" />
             <meta name="og:description"
-                content="Vivek Patel's (vivek9patel) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+                content="Nikola Dimitro's Personal Portfolio Website. Made with Ubuntu 20.04 (Linux) theme by Next.js and Tailwind CSS." />
             <meta name="og:image" content="images/logos/logo_1200.png" />
-            <meta name="og:url" content="http://vivek9patel.github.io/" />
-            <meta name="og:site_name" content="Vivek Patel Personal Portfolio" />
-            <meta name="og:locale" content="en_IN" />
+            <meta name="og:url" content="https://NikolaDimitro.github.io/" />
+            <meta name="og:site_name" content="Nikola Dimitro Personal Portfolio" />
+            <meta name="og:locale" content="en_US" />
             <meta name="og:type" content="website" />
 
             <link rel="icon" href="images/logos/fevicon.svg" />
